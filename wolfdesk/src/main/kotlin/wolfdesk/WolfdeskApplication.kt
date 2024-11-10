6dfde7e219d0e945ac5b2bfed729963f5592c5c1
nooose <hd15807@gmail.com>
2024-11-10 19:09:01 +0900

    fix: ConfigurationPropertiesScan 추가

--- wolfdesk/src/main/kotlin/wolfdesk/WolfdeskApplication.kt
+++ wolfdesk/src/main/kotlin/wolfdesk/WolfdeskApplication.kt
@@ -1,10 +1,10 @@
 package wolfdesk
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class WolfdeskApplication
 

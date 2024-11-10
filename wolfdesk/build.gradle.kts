a7a5e993a9cf93e5c431d7e1a0f82b70de4e03a0
nooose <hd15807@gmail.com>
2024-11-10 17:28:50 +0900

    refactor: JWT 의존성 base 모듈로 이동

--- wolfdesk/build.gradle.kts
+++ wolfdesk/build.gradle.kts
@@ -24,7 +24,6 @@ configurations {
 val kotestVersion = "5.9.1"
 val kotestSpringVersion = "1.3.0"
 val jdslVersion = "3.5.2"
-val jjwtVersion = "0.12.6"
 val mockkVersion = "1.13.10"
 
 subprojects {
@@ -67,6 +66,7 @@ subprojects {
             exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
             exclude(group = "org.mockito")
         }
+        testImplementation("io.mockk:mockk:${mockkVersion}")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
         testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
@@ -96,6 +96,7 @@ dependencies {
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+    testImplementation("io.mockk:mockk:${mockkVersion}")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(group = "org.mockito")
